ab2654004ff56f4da33a24b4dc53ec4c8340cc1c
juanm <juanm@DESKTOP-CQ79OHG>
2022-11-15 23:46:00 +0100

    Exercise04 before comment

---
+++ EjerciciosT3-GallegoGiron/src/ejercicios/Ejercicio04.java
@@ -0,0 +1,58 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio04 {
+
+	public static void main(String[] args) {
+
+		int numA, numB;
+		int numMax = 0;
+		int mcd = 0;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Introduzca un número: ");
+		numA = sc.nextInt();
+		System.out.print("Introduzca otro número: ");
+		numB = sc.nextInt();
+		System.out.println();
+
+		if (numA <= 0 || numB <= 0) {
+			
+			System.out.println("ERROR: Introduce valores enteros positivos y mayores que 0");
+			
+		} else {
+
+			if (numA == numB) {
+				
+				mcd = numA;
+				
+			} else {
+				
+				if (numA > numB)
+
+					numMax = numA;
+
+				else if (numA < numB)
+					
+					numMax = numB;
+
+				for (int i = numMax; numA % i != 0 || numB % i != 0; i--) {
+
+					mcd = i;
+					mcd--;
+					
+				}
+				
+			}
+			
+			System.out.println("El máximo común divisor es: " + mcd);
+			
+		}
+
+		sc.close();
+		
+	}
+
+}
